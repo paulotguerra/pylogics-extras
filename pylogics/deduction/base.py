ba92df811a2588572a6e91c7eef37769b9e6f48b
Paulo T. Guerra <paulodetarso@ufc.br>
2023-12-29 16:45:01 -0300

    Minor adjustments.

--- pylogics/deduction/base.py
+++ pylogics/deduction/base.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2021 WhiteMech
+# Copyright 2023 
 #
 # ------------------------------
 #
-# This file is part of pylogics.
+# This file is part of pylogics-extras.
 #
-# pylogics is free software: you can redistribute it and/or modify
+# pylogics-extras is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# pylogics is distributed in the hope that it will be useful,
+# pylogics-extras is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with pylogics.  If not, see <https://www.gnu.org/licenses/>.
+# along with pylogics-extras.  If not, see <https://www.gnu.org/licenses/>.
 #
 
 """Base classes for pylogics deduction systems."""
